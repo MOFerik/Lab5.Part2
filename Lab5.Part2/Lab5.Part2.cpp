ec2f3b30f4a9352f07908555a479611ca162eeb9
MOFerik <73346667+MOFerik@users.noreply.github.com>
2020-12-18 03:14:50 +0500

    Функции преведения типов
    Функции ручного и динамического преведения типов объектов Base к Desc

--- Lab5.Part2/Lab5.Part2.cpp
+++ Lab5.Part2/Lab5.Part2.cpp
@@ -22,7 +22,7 @@ public: int baseCheck = 0;
 
 	virtual string classname()
 	{
-		return "Object's class is Base.\n";
+		return "Base";
 	}
 
 	virtual bool isA(string clsname)
@@ -64,7 +64,7 @@ class Desc : public Base
 
 	string classname()
 	{
-		return "Object's class is Desc.\n";
+		return "Desc";
 	}
 
 	bool isA(string clsname)
@@ -130,10 +130,37 @@ void desc1Ops()
 	cout << "\nDestroying desc1:\n";
 }
 
+void funcDynamicCast()
+{
+	cout << "\n\nCreating Desc-class Object desc2:\n";
+	Desc* desc2 = new Desc;
+	cout << "\nCreating Base-class Object base2 and dynamicly cassting it to Desc-class type:\n";
+	Base* base2 = new Base;
+	base2 = dynamic_cast<Base*>(desc2);
+
+	cout << "\nChecking base2's type using isA('Desc'): \n";
+	base2->isA("Desc");
+}
+
+void funcManualCast()
+{
+	cout << "\n\nCreating Desc-class Object desc3:\n";
+	Desc* desc3 = new Desc;
+	cout << "\nCreating Base-class Object base3 and manually cassting it to Desc-class type:\n";
+	Base* base3 = new Base;
+	base3 = ((Base*)(desc3));
+
+	cout << "\nChecking base3's type using isA('Desc'): \n";
+	base3->isA("Desc");
+}
+
 int main()
 {
 	base1Ops();
 
 	desc1Ops();
 
+	funcDynamicCast();
+
+	funcManualCast();
 }
